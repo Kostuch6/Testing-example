1d1163f8c1e2c4c54355f3f759eb0d66eae79174
Patryk K <patryk.koc44@gmail.com>
2025-05-09 12:19:22 +0200

    starting paperTest

---
+++ src/test/java/com/learning/courses/model/PaperTest.java
@@ -0,0 +1,31 @@
+package com.learning.courses.model;
+
+
+import org.junit.jupiter.api.Test;
+
+public class PaperTest {
+
+
+
+
+    @Test
+    void shouldCreatePaperWithValidData() {
+        // Test that a paper object is created with correct attributes
+    }
+
+    @Test
+    void shouldAllowMultipleAuthorsInPaper() {
+        // Test that the paper correctly stores a list of authors
+    }
+
+    @Test
+    void shouldHandleMissingOptionalFields() {
+        // Test paper creation when some optional fields like content are missing
+    }
+
+    @Test
+    void shouldRejectInvalidISBNFormat() {
+        // Test handling of invalid ISBN format (if validation is added)
+    }
+
+}
