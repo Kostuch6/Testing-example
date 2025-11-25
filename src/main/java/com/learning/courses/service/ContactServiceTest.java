964c6fb3ccc203d18920ca9aca5be908c139bb3f
pawel <figson94@gmail.com>
2025-11-25 13:03:01 +0100

    kontakt do studenta

--- src/main/java/com/learning/courses/service/ContactServiceTest.java
+++ src/main/java/com/learning/courses/service/ContactServiceTest.java
@@ -11,27 +11,3 @@ class ContactServiceTest {
 
     }
 
-    @Test
-    @DisplayName("should not create contact for non existing person")
-    void shouldNotCreateContactForNonExistingPerson() {
-
-    }
-
-    @Test
-    @DisplayName("should not create contact for tutor")
-    void shouldNotCreateContactForTutor() {
-
-    }
-
-    @Test
-    @DisplayName("should update existing contact")
-    void shouldUpdateExistingContact() {
-
-    }
-
-    @Test
-    @DisplayName("should delete existing contact")
-    void shouldDeleteExistingContact() {
-
-    }
-}
